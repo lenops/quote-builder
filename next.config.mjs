377ed06761d7f51b84bf1dee892600d4510076eb
lenops <37666363+lenops@users.noreply.github.com>
2025-09-10 10:16:44 +0100

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+// File: next.config.mjs
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+reactStrictMode: true,
+experimental: {
+reactCompiler: true
+}
+};
+export default nextConfig;
